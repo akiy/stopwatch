07ad92d995fea1e0f4a8c478c11b8c6ca3670736
akira <rafael.akkira@gmail.com>
2021-03-15 11:46:56 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,70 @@
+const minutesInput = document.querySelector('.minutes');
+const secondsInput = document.querySelector('.seconds');
+const millisecondsInput = document.querySelector('.milliseconds');
+const dots = document.querySelectorAll('.dots');
+
+const startButton = document.querySelector('.btn-start');
+const stopButton = document.querySelector('.btn-stop');
+const resetButton = document.querySelector('.btn-reset');
+
+millisecondsInput.textContent = '00';
+secondsInput.textContent = '00';
+minutesInput.textContent = '00';
+
+dots.forEach(dots => {
+    dots.textContent = ' : ';
+})
+
+
+let minutes = 00;
+let seconds = 00;
+let milliseconds = 00;
+
+let stopWatch;
+
+const renderFormat = function(elapsedTime) {
+    return elapsedTime < 10 ? `0${elapsedTime}` : `${elapsedTime}`;
+};
+
+const timeRunning = function() {
+  
+    milliseconds++;
+    millisecondsInput.textContent = renderFormat(milliseconds);
+    
+   
+    if (milliseconds > 99) {
+        milliseconds = 0;
+        seconds++;
+        secondsInput.textContent = renderFormat(seconds);
+    }
+
+    if (seconds > 59) {
+        seconds = 0
+        minutes++; 
+        minutesInput.textContent = renderFormat(minutes);
+    }
+};
+
+const startStopWatch = function() {
+    clearInterval(stopWatch);
+    stopWatch = setInterval(timeRunning, 10);
+};
+
+
+const stopStopWatch = function() {
+    clearInterval(stopWatch);
+}
+
+const resetStopWatch = function() {
+    clearInterval(stopWatch);
+    milliseconds = 0;
+    seconds = 0;
+    minutes = 0;
+    millisecondsInput.textContent = '00';
+    secondsInput.textContent = '00';
+    minutesInput.textContent = '00';
+}
+
+startButton.addEventListener('click', startStopWatch);
+stopButton.addEventListener('click', stopStopWatch);
+resetButton.addEventListener('click', resetStopWatch);
